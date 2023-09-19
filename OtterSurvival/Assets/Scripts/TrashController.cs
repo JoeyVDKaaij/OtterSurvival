8c91fbaef1b75b6f3365b632fabb9c36d5d1da2b
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2023-09-19 10:54:50 +0200

    Finished enemy script + Low air volume alarm changes
    Enemy script works perfectly + prefab made to play around with.
    
    Low alarm is now updated to use weight instead of intensity of the vignette
    
    Not applying the slider to the airscripts doesn't give you null errors

--- OtterSurvival/Assets/Scripts/TrashController.cs
+++ OtterSurvival/Assets/Scripts/TrashController.cs
@@ -8,7 +8,6 @@ public class TrashController : MonoBehaviour
     // Decrease more air given by the number above or in the inspector
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Hit");
         if (other.gameObject.CompareTag("Player")) other.gameObject.GetComponent<AirController>().AddedDecreaseAirSpeed(addedDecreaseAirTime);
     }
 }
