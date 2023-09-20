a54bb5aac410918ad00d2f3db46724bfce8fa359
Joey van der Kaaij <joey.vanderkaaij@gmail.nl>
2023-09-20 22:36:11 +0200

    Victory animation + menu update

--- OtterSurvival/Assets/Scripts/MenuScript.cs
+++ OtterSurvival/Assets/Scripts/MenuScript.cs
@@ -1,8 +1,15 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
