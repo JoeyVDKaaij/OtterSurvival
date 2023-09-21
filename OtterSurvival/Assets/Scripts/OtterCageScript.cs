5f8057c90e5697e0b5df3f681b2b714b3e840d7a
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2023-09-21 19:28:43 +0200

    Hud update, Particle system and transition to the right scenes

--- OtterSurvival/Assets/Scripts/OtterCageScript.cs
+++ OtterSurvival/Assets/Scripts/OtterCageScript.cs
@@ -5,12 +5,20 @@ public class OtterCageScript : MonoBehaviour
     [SerializeField, Tooltip("Set the pipe that gets affected by the unlocking of the cage")]
     private GameObject affectedPipe;
 
+    [SerializeField, Tooltip("Set the otter that gets disabled by the unlocking of the cage")]
+    private GameObject otter;
+
     [Tooltip("The item that is needed to open the cage")]
     public ItemObject item;
+
+    [SerializeField, Tooltip("The particle that plays when you open the cage")]
+    private ParticleSystem particles;
 
     public void GateOpen()
     {
         // Let the pipe Script know it can close and do it's closing effect
+        particles.Play();
+        otter.SetActive(false);
         affectedPipe.GetComponent<PipeScript>().conditionsMet = true;
         Destroy(gameObject);
     }
